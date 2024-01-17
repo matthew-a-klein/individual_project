324b7337515ad24dcbf6e9258cd93bc08ee6c40f
Matthew Klein <matthew-a-klein@github.com>
2024-01-17 13:59:00 +0000

    Add regex matcher and Lexer

---
+++ src/lexer.rs
@@ -0,0 +1,236 @@
+use crate::reg::nullable;
+use crate::reg::Re::*;
+use crate::reg::*;
+use Val::*;
+#[derive(Debug, Clone, PartialEq)]
+pub enum Val {
+    Empty,
+    Chr(char),
+    Sequ(Box<Val>, Box<Val>),
+    Left(Box<Val>),
+    Right(Box<Val>),
+    Stars(Vec<Val>),
+    // Extended Regular Expressions
+    Rec(String, Box<Val>),
+}
+fn flatten(v: &Val) -> String {
+    match v {
+        Empty => String::new(),
+        Chr(c) => c.to_string(),
+        Left(v) => flatten(&*v),
+        Right(v) => flatten(&*v),
+        Sequ(v1, v2) => flatten(&*v1) + &flatten(&*v2),
+        Stars(v) => v
+            .iter()
+            .map(|v| flatten(&v))
+            .collect::<Vec<String>>()
+            .join(""), // Handle other cases accordingly
+        Rec(_, v) => flatten(v),
+    }
+}
+
+pub fn env(v: Val) -> Vec<(String, String)> {
+    match v {
+        Empty => Vec::new(),
+        Chr(_) => Vec::new(),
+        Left(v) => env(*v),
+        Right(v) => env(*v),
+        Sequ(v1, v2) => {
+            let mut result = env(*v1);
+            result.extend(env(*v2));
+            result
+        }
+        Stars(v) => v.into_iter().flat_map(|v| env(v)).collect(),
+        Rec(r, v) => {
+            let mut result: Vec<(String, String)> = Vec::new();
+            result.push((r, flatten(&v)));
+            result.extend(env(*v));
+            result
+        }
+    }
+}
+fn mkeps(r: &Re) -> Val {
+    match r {
+        ZERO => panic!(),
+        ONE => Empty,
+        CHAR(_) => panic!(),
+        ALT(l, r) => {
+            if nullable(l) {
+                Left(Box::new(mkeps(l)))
+            } else {
+                Right(Box::new(mkeps(r)))
+            }
+        }
+        SEQ(v1, v2) => Sequ(Box::new(mkeps(v1)), Box::new(mkeps(v2))),
+        STAR(_) => Stars(Vec::new()),
+        RECD(s, r) => Rec(s.to_string(), Box::new(mkeps(r))),
+    }
+}
+
+pub fn inj(r: &Re, c: char, v: Val) -> Val {
+    match (r, v) {
+        (CHAR(_), Empty) => Chr(c),
+
+        (ALT(r1, _), Left(v1)) => Left(Box::new(inj(r1, c, *v1))),
+        (ALT(_, r2), Right(v2)) => Right(Box::new(inj(r2, c, *v2))),
+
+        (SEQ(r1, _), Sequ(v1, v2)) => Sequ(Box::new(inj(r1, c, *v1)), v2),
+        (SEQ(r1, _), Left(v)) => match *v {
+            Sequ(v1, v2) => Sequ(Box::new(inj(r1, c, *v1)), v2),
+            _ => panic!(),
+        },
+        (SEQ(r1, r2), Right(v)) => match *v {
+            v => Sequ(Box::new(mkeps(&*r1)), Box::new(inj(&*r2, c, v))),
+            _ => {
+                println!("{:?}", *v);
+                panic!()
+            }
+        },
+
+        (STAR(r), Sequ(v1, v2)) => match *v2 {
+            Stars(vs) => {
+                let mut res = Vec::new();
+                res.push(inj(r, c, *v1));
+                res.extend(vs);
+                Stars(res)
+            }
+            _ => panic!(),
+        },
+        // case (RECD(x, r1), _) => Rec(x, inj(r1, c, v))
+        (RECD(s, r), Rec(_, v)) => Rec(s.to_string(), Box::new(inj(r, c, *v))),
+        (r, v) => {
+            println!("{:?},   {:?}", r, v);
+            panic!()
+        }
+    }
+}
+
+fn f_id() -> Box<dyn Fn(Val) -> Val> {
+    Box::new(move |v| v)
+}
+
+fn f_right<'a, F: 'a>(f: F) -> Box<dyn Fn(Val) -> Val + 'a>
+where
+    F: Fn(Val) -> Val,
+{
+    Box::new(move |v| Right(Box::new(f(v))))
+}
+
+fn f_left<'a, F: 'a>(f: F) -> Box<dyn Fn(Val) -> Val + 'a>
+where
+    F: Fn(Val) -> Val,
+{
+    Box::new(move |v| Left(Box::new(f(v))))
+}
+
+fn f_alt<'a, F1: 'a, F2: 'a>(f1: F1, f2: F2) -> Box<dyn Fn(Val) -> Val + 'a>
+where
+    F1: Fn(Val) -> Val,
+    F2: Fn(Val) -> Val,
+{
+    Box::new(move |v| match v {
+        Left(v) => Left(Box::new(f1(*v))),
+        Right(v) => Right(Box::new(f2(*v))),
+        _ => panic!(),
+    })
+}
+
+fn f_seq<'a, F1: 'a, F2: 'a>(f1: F1, f2: F2) -> Box<dyn Fn(Val) -> Val + 'a>
+where
+    F1: Fn(Val) -> Val,
+    F2: Fn(Val) -> Val,
+{
+    Box::new(move |v| match v {
+        Sequ(v1, v2) => Sequ(Box::new(f1(*v1)), Box::new(f2(*v2))),
+        _ => panic!(),
+    })
+}
+
+fn f_seq_empty_1<'a, F1: 'a, F2: 'a>(f1: F1, f2: F2) -> Box<dyn Fn(Val) -> Val + 'a>
+where
+    F1: Fn(Val) -> Val,
+    F2: Fn(Val) -> Val,
+{
+    Box::new(move |v| match v {
+        v => Sequ(Box::new(f1(Empty)), Box::new(f2(v))),
+    })
+}
+
+fn f_seq_empty_2<'a, F1: 'a, F2: 'a>(f1: F1, f2: F2) -> Box<dyn Fn(Val) -> Val + 'a>
+where
+    F1: Fn(Val) -> Val,
+    F2: Fn(Val) -> Val,
+{
+    Box::new(move |v| match v {
+        v => Sequ(Box::new(f1(v)), Box::new(f2(Empty))),
+    })
+}
+
+fn f_recd<'a, F: 'a>(f: F) -> Box<dyn Fn(Val) -> Val + 'a>
+where
+    F: Fn(Val) -> Val,
+{
+    Box::new(move |v| match v {
+        Rec(x, v) => Rec(x, Box::new(f(*v))),
+        _ => panic!(),
+    })
+}
+
+fn f_error() -> Box<dyn Fn(Val) -> Val> {
+    Box::new(move |v| panic!())
+}
+
+// Simplify function
+fn simp(r: Re) -> (Re, impl Fn(Val) -> Val) {
+    match r {
+        ALT(r1, r2) => {
+            let (r1s, f1s) = simp(*r1);
+            let (r2s, f2s) = simp(*r2);
+            match (&r1s, &r2s) {
+                (ZERO, _) => (r2s, f_right(f2s)),
+                (_, ZERO) => (r1s, f_left(f1s)),
+                _ if r1s == r2s => (r1s, f_left(f1s)),
+                _ => (ALT(Box::new(r1s), Box::new(r2s)), f_alt(f1s, f2s)),
+            }
+        }
+        SEQ(r1, r2) => {
+            let (r1s, f1s) = simp(*r1);
+            let (r2s, f2s) = simp(*r2);
+            match (&r1s, &r2s) {
+                (ZERO, _) => (ZERO, f_error()),
+                (_, ZERO) => (ZERO, f_error()),
+                (ONE, _) => (r2s, f_seq_empty_1(f1s, f2s)),
+                (_, ONE) => (r1s, f_seq_empty_2(f1s, f2s)),
+
+                _ => (SEQ(Box::new(r1s), Box::new(r2s)), f_seq(f1s, f2s)),
+            }
+        }
+
+        r => (r, f_id()),
+    }
+}
+
+pub fn lex_simp(r: &Re, s: Vec<char>) -> Val {
+    if let Some((c, cs)) = s.split_first() {
+        let (r_simp, f_simp) = simp(der(*c, r));
+        inj(r, *c, f_simp(lex_simp(&r_simp, cs.to_vec())))
+    } else {
+        if nullable(r) {
+            mkeps(r)
+        } else {
+            panic!("lexing error")
+        }
+    }
+}
+
+pub fn ders_simp(cs: Vec<char>, r: Re) -> Re {
+    if let Some((c, cs)) = cs.split_first() {
+        ders_simp(cs.to_vec(), simp(der(*c, &r)).0)
+    } else {
+        r
+    }
+}
+
+pub fn lexing_simp(r: &Re, s: &str) -> Vec<(String, String)> {
+    env(lex_simp(r, s.chars().collect()))
+}
---
+++ src/reg.rs
@@ -0,0 +1,139 @@
+use std::iter::Once;
+
+use Re::*;
+#[derive(Debug, Clone, PartialEq)]
+pub enum Re {
+    ZERO,
+    ONE,
+    CHAR(char),
+    ALT(Box<Re>, Box<Re>),
+    SEQ(Box<Re>, Box<Re>),
+    STAR(Box<Re>),
+    RECD(String, Box<Re>),
+}
+
+pub fn nullable(r: &Re) -> bool {
+    match r {
+        ZERO => false,
+        ONE => true,
+        CHAR(_) => false,
+        ALT(l, r) => nullable(&*l) || nullable(&*r),
+        SEQ(r1, r2) => nullable(&*r1) && nullable(&*r2),
+        STAR(_) => true,
+        RECD(_, r) => nullable(r),
+    }
+}
+
+pub fn der(c: char, re: &Re) -> Re {
+    match re {
+        ZERO => ZERO,
+        ONE => ZERO,
+        CHAR(d) => {
+            if c == *d {
+                ONE
+            } else {
+                ZERO
+            }
+        }
+        ALT(l, r) => ALT(Box::new(der(c, &*l)), Box::new(der(c, &*r))), // Wrapped with Box::new
+        SEQ(r1, r2) => {
+            if nullable(&*r1) {
+                ALT(
+                    Box::new(SEQ(Box::new(der(c, &*r1)), Box::new(*r2.clone()))),
+                    Box::new(der(c, &*r2)),
+                )
+            } else {
+                SEQ(Box::new(der(c, &*r1)), Box::new(*r2.clone()))
+            }
+        }
+        STAR(r) => SEQ(Box::new(der(c, r)), Box::new(STAR(Box::new(*r.clone())))),
+        RECD(s, r) => RECD(s.to_string(), Box::new(der(c, r))),
+    }
+}
+
+fn simp(re: &Re) -> &Re {
+    match re {
+        ALT(l, r) => match (simp(l), simp(r)) {
+            (ZERO, rs) => rs,
+            (ls, ZERO) => ls,
+            (ls, rs) => {
+                if rs == ls {
+                    ls
+                } else {
+                    re
+                }
+            }
+        },
+        SEQ(r1, r2) => match (simp(r1), simp(r2)) {
+            (ZERO, _) => &ZERO,
+            (_, ZERO) => &ZERO,
+            (ONE, r2) => r2,
+            (r1, ONE) => r1,
+            (_, _) => re,
+        },
+        re => re,
+    }
+}
+
+pub fn ders<'a>(s: &'a str, re: &'a Re) -> Re {
+    match s.chars().next() {
+        None => re.clone(),
+        Some(c) => {
+            let derived = der(c, re);
+            let simplified = simp(&derived);
+            ders(&s[1..], simplified)
+        }
+    }
+}
+
+pub fn matcher(r: &Re, s: &str) -> bool {
+    nullable(&ders(s, r))
+}
+
+pub fn char(c: char) -> Re {
+    CHAR(c)
+}
+
+pub fn concat(re1: Re, re2: Re) -> Re {
+    SEQ(Box::new(re1), Box::new(re2))
+}
+
+pub fn alt(re1: Re, re2: Re) -> Re {
+    ALT(Box::new(re1), Box::new(re2))
+}
+
+pub fn star(re: Re) -> Re {
+    STAR(Box::new(re))
+}
+
+pub fn recd(s: &str, r: Re) -> Re {
+    RECD(s.to_string(), Box::new(r))
+}
+
+impl std::ops::Add for Re {
+    type Output = Re;
+
+    fn add(self, rhs: Re) -> Re {
+        concat(self, rhs)
+    }
+}
+
+impl std::ops::BitOr for Re {
+    type Output = Re;
+
+    fn bitor(self, rhs: Re) -> Re {
+        alt(self, rhs)
+    }
+}
+
+pub fn char_list_to_rexp(s: &[char]) -> Re {
+    match s {
+        [] => ONE,
+        [c] => char(*c),
+        [c, rest @ ..] => SEQ(Box::new(char(*c)), Box::new(char_list_to_rexp(rest))),
+    }
+}
+
+pub fn string_to_rexp(s: &str) -> Re {
+    char_list_to_rexp(s.chars().collect::<Vec<char>>().as_slice())
+}
